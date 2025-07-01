d7f2c4c2c17c9ee0bb839d85018fd1b6589f92f0
Karesse DSouza <kadsouza+cisco@cisco.com>
2025-07-01 13:52:22 +0530

    first commit

---
+++ src/main/java/dev/karesse/movies/model/Review.java
@@ -0,0 +1,18 @@
+package dev.karesse.movies.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    @Id
+    private Object id;
+    private String body;
+}
